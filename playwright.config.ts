91a13ed82aee856724c97dec8d2dc758e13a3555
Misha Pisaryk <333mike555@gmail.com>
2025-09-19 18:09:07 +0200

    initial draft of tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,35 +2,36 @@ import { defineConfig, devices } from '@playwright/test';
 import dotenv from 'dotenv';
 import * as path from 'path';
 
-const ENV = process.env.ENVIRONMENT || 'prod';
+const ENV = process.env.ENVIRONMENT || 'dev';
 const envPath = path.resolve(__dirname, `./env/.env.${ENV}`);
 dotenv.config({ path: envPath });
 
 export default defineConfig({
-  testDir: './tests',
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
-  use: {
-    trace: 'on-first-retry',
-  },
-
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+    testDir: './tests',
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 2 : 0,
+    workers: process.env.CI ? 1 : undefined,
+    reporter: 'html',
+    use: {
+        baseURL: process.env.BASE_URL,
+        trace: 'on-first-retry',
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+        },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-  ],
+        {
+            name: 'firefox',
+            use: { ...devices['Desktop Firefox'] },
+        },
+
+        {
+            name: 'webkit',
+            use: { ...devices['Desktop Safari'] },
+        },
+    ],
 });
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,3 +1,86 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-test.describe('UI Tests Examples', () => {});
+test.describe('UI Tests Examples', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/');
+    });
+
+    test('Overlapped Element test', async ({ page }) => {
+        const overlappedElement = page.locator('h3 > a').filter({ hasText: 'Overlapped Element' });
+        await overlappedElement.click();
+        const overlappedElementHeader = page.getByRole('heading', { name: 'Overlapped Element' });
+        await expect(overlappedElementHeader).toBeVisible();
+
+        const inputNameField = page.getByPlaceholder('Name');
+        const elementHandle = await inputNameField.elementHandle();
+        await page.evaluate((el) => el.scrollIntoView({ block: 'center' }), elementHandle);
+        // await inputNameField.hover();
+        // await page.mouse.wheel(0, 16);
+        await expect(inputNameField).toBeVisible();
+        await inputNameField.fill('Misha');
+        await expect(inputNameField).toHaveValue('Misha');
+    });
+
+    test('AJAX Data test', async ({ page }) => {
+        const ajaxData = page.locator('h3 > a').filter({ hasText: 'AJAX DATA' });
+        await ajaxData.click();
+        const ajaxDataHeader = page.getByRole('heading', { name: 'AJAX Data' });
+        await expect(ajaxDataHeader).toBeVisible();
+
+        const buttonAjaxTrigger = page.getByRole('button', {
+            name: 'Button Triggering AJAX Request',
+        });
+        await buttonAjaxTrigger.click();
+        const successLabel = page
+            .locator('#content p')
+            .filter({ hasText: 'Data loaded with AJAX get request.' });
+        await expect(successLabel).toBeVisible({ timeout: 16000 });
+        await successLabel.click();
+    });
+
+    test('Visibility test', async ({ page }) => {
+        const visibility = page.locator('h3 > a').filter({ hasText: 'Visibility' });
+        await visibility.click();
+        const hideButton = page.getByRole('button', { name: 'Hide' });
+        const removedButton = page.getByRole('button', { name: 'Removed' });
+        const zeroWidthButton = page.getByRole('button', { name: 'Zero Width' });
+        const overlappedButton = page.getByRole('button', { name: 'Overlapped' });
+        const opacity0Button = page.getByRole('button', { name: 'Opacity 0' });
+        const visibilityHiddenButton = page.getByRole('button', { name: 'Visibility Hidden' });
+        const displayNoneButton = page.getByRole('button', { name: 'Display None' });
+        const offscreenButton = page.getByRole('button', { name: 'Offscreen' });
+
+        // Click Hide button to trigger visibility changes
+        await hideButton.click();
+
+        // Check visibility of buttons
+        await expect(hideButton).toBeVisible(); // Hide button remains visible
+
+        await expect(removedButton).toBeHidden();
+        await expect(zeroWidthButton).toBeHidden();
+        //1
+        const overlappedButtonBox = await overlappedButton.boundingBox();
+        const centerX = overlappedButtonBox.x + overlappedButtonBox.width / 2;
+        const centerY = overlappedButtonBox.y + overlappedButtonBox.height / 2;
+        const topElementId = await page.evaluate(
+            ({ x, y }) => {
+                return document.elementFromPoint(x, y)?.id;
+            },
+            { x: centerX, y: centerY },
+        );
+        expect(topElementId).not.toBe('overlappedButton');
+        await expect(async () => {
+            await overlappedButton.click({ trial: true });
+        }).rejects.toThrow(); // check that the button cannot be clicked by a user
+
+        //!!!!fix the rest of buttons
+        await expect(opacity0Button).toHaveCount(1);
+        await expect(opacity0Button).toHaveCSS('opacity', '0');
+        await expect(async () => {
+            await opacity0Button.click({ trial: true });
+        }).rejects.toThrow();
+        await expect(visibilityHiddenButton).toBeHidden();
+        await expect(displayNoneButton).toBeHidden();
+        await expect(offscreenButton).toBeHidden();
+    });
+});
