ea48aeaf01ac7058ad13a43f593d7f42fd6bf890
Misha Pisaryk <333mike555@gmail.com>
2025-09-21 19:07:36 +0200

    Visibility fixes

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,7 +6,7 @@ test.describe('UI Tests Examples', () => {
     });
 
     test('Overlapped Element', async ({ page }) => {
-        const overlappedElement = page.locator('h3 > a').filter({ hasText: 'Overlapped Element' });
+        const overlappedElement = page.getByRole('link', { name: 'Overlapped Element' });
         await overlappedElement.click();
         const overlappedElementHeader = page.getByRole('heading', { name: 'Overlapped Element' });
         await expect(overlappedElementHeader).toBeVisible();
@@ -22,7 +22,7 @@ test.describe('UI Tests Examples', () => {
     });
 
     test('AJAX Data', async ({ page }) => {
-        const ajaxData = page.locator('h3 > a').filter({ hasText: 'AJAX DATA' });
+        const ajaxData = page.getByRole('link', { name: 'AJAX DATA' });
         await ajaxData.click();
         const ajaxDataHeader = page.getByRole('heading', { name: 'AJAX Data' });
         await expect(ajaxDataHeader).toBeVisible();
@@ -39,9 +39,13 @@ test.describe('UI Tests Examples', () => {
     });
 
     test('Visibility', async ({ page }) => {
-        const visibility = page.locator('h3 > a').filter({ hasText: 'Visibility' });
+        const visibility = page.getByRole('link', { name: 'Visibility' });
         await visibility.click();
         const hideButton = page.getByRole('button', { name: 'Hide' });
+
+        // Click Hide button to trigger visibility changes
+        await hideButton.click();
+
         const removedButton = page.getByRole('button', { name: 'Removed' });
         const zeroWidthButton = page.getByRole('button', { name: 'Zero Width' });
         const overlappedButton = page.getByRole('button', { name: 'Overlapped' });
@@ -50,15 +54,10 @@ test.describe('UI Tests Examples', () => {
         const displayNoneButton = page.getByRole('button', { name: 'Display None' });
         const offscreenButton = page.getByRole('button', { name: 'Offscreen' });
 
-        // Click Hide button to trigger visibility changes
-        await hideButton.click();
-
         // Check visibility of buttons
         await expect(hideButton).toBeVisible(); // Hide button remains visible
-
         await expect(removedButton).toBeHidden();
         await expect(zeroWidthButton).toBeHidden();
-        //1
         const overlappedButtonBox = await overlappedButton.boundingBox();
         const centerX = overlappedButtonBox.x + overlappedButtonBox.width / 2;
         const centerY = overlappedButtonBox.y + overlappedButtonBox.height / 2;
@@ -69,19 +68,10 @@ test.describe('UI Tests Examples', () => {
             { x: centerX, y: centerY },
         );
         expect(topElementId).not.toBe('overlappedButton');
-        await expect(async () => {
-            await overlappedButton.click({ trial: true });
-        }).rejects.toThrow(); // check that the button cannot be clicked by a user
-
-        //!!!!fix the rest of buttons
-        await expect(opacity0Button).toHaveCount(1);
         await expect(opacity0Button).toHaveCSS('opacity', '0');
-        await expect(async () => {
-            await opacity0Button.click({ trial: true });
-        }).rejects.toThrow();
         await expect(visibilityHiddenButton).toBeHidden();
         await expect(displayNoneButton).toBeHidden();
-        await expect(offscreenButton).toBeHidden();
+        await expect(offscreenButton).not.toBeInViewport();
     });
 
     test('Dynamic Table', async ({ page }) => {
@@ -108,7 +98,7 @@ test.describe('UI Tests Examples', () => {
 
     // note for me - rewrite with env variables
     test('Sample App', async ({ page }) => {
-        const sampleApp = page.locator('h3 > a').filter({ hasText: 'Sample App' });
+        const sampleApp = page.getByRole('link', { name: 'Sample App' });
         await sampleApp.click();
         const sampleAppHeader = page.getByRole('heading', { name: 'Sample App' });
         await expect(sampleAppHeader).toBeVisible();
@@ -131,7 +121,7 @@ test.describe('UI Tests Examples', () => {
     });
 
     test('Text Input', async ({ page }) => {
-        const textInput = page.locator('h3 > a').filter({ hasText: 'Text Input' });
+        const textInput = page.getByRole('link', { name: 'Text Input' });
         await textInput.click();
         const textInputHeader = page.getByRole('heading', { name: 'Text Input' });
         await expect(textInputHeader).toBeVisible();
@@ -146,7 +136,7 @@ test.describe('UI Tests Examples', () => {
     });
 
     test('Progress Bar', async ({ page }) => {
-        const textInput = page.locator('h3 > a').filter({ hasText: 'Progress Bar' });
+        const textInput = page.getByRole('link', { name: 'Progress Bar' });
         await textInput.click();
         const textInputHeader = page.getByRole('heading', { name: 'Progress Bar' });
         await expect(textInputHeader).toBeVisible();
