a7ba5c25bb42e56035114f991a5cee4df4fdbb55
Misha Pisaryk <333mike555@gmail.com>
2025-09-23 14:06:19 +0200

    Added Allure Reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? 1 : undefined,
-    reporter: 'html',
+    reporter: [['html'], ['allure-playwright']],
     use: {
         baseURL: process.env.BASE_URL,
         trace: 'on-first-retry',
